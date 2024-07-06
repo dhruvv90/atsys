55e9e19a2ff99b44d132ad90b77c06bd54030c7e
Dhruv Verma <dhruvv90@gmail.com>
2024-07-06 22:39:14 +0530

    Cleanup

--- src/main/java/atsys/backtesting/components/strategy/Strategy.java
+++ src/main/java/atsys/backtesting/components/strategy/Strategy.java
@@ -1,13 +1,8 @@
 package atsys.backtesting.components.strategy;
 
-import atsys.backtesting.engine.BacktestingContext;
 import atsys.backtesting.components.LifecycleManager;
 import atsys.backtesting.engine.events.TickEvent;
 
 public interface Strategy extends LifecycleManager {
-
-    void onInit(BacktestingContext context);
     void handleTick(TickEvent event);
-    void onComplete();
-
 }

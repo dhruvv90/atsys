6820b99532407820d47b5ee83e340f8607ffbe07
Dhruv Verma <dhruvv90@gmail.com>
2024-07-06 17:41:03 +0530

    Merge engine into backtester

--- src/main/java/atsys/backtesting/Backtester.java
+++ src/main/java/atsys/backtesting/Backtester.java
@@ -3,26 +3,37 @@ package atsys.backtesting;
 import atsys.backtesting.engine.*;
 import atsys.backtesting.engine.event.Event;
 import atsys.backtesting.engine.event.TickEvent;
+import atsys.backtesting.engine.listeners.TickEventListener;
 import atsys.backtesting.model.TickData;
 import atsys.backtesting.components.data.TickDataStreamer;
+import lombok.Getter;
 
 
 public class Backtester {
 
-    private final EventDrivenEngine engine;
+    private final EventQueue<Event> eventQueue;
+    private final EventConsumer eventConsumer;
+    private final EventPublisher eventPublisher;
+    private final EventEmitter eventEmitter;
 
 
     public Backtester() {
-        this.engine = new EventDrivenEngine();
+        this.eventQueue = new EventQueueImpl();
+        this.eventEmitter = new EventEmitter();
+
+        this.eventConsumer = new EventConsumer(eventQueue);
+        this.eventPublisher = new EventPublisher(eventQueue);
+    }
+
+    private void reset() {
+        this.eventQueue.clear();
     }
 
     public void run(Backtest bt) {
         TickDataStreamer dataStreamer = new TickDataStreamer();
 
-        engine.registerStrategy(bt.getStrategy());
-
-        EventConsumer eventConsumer = engine.getEventConsumer();
-        EventPublisher eventPublisher = engine.getEventPublisher();
+        TickEventListener tickListener = new TickEventListener(bt.getStrategy());
+        eventEmitter.register(TickEvent.class, tickListener);
 
         bt.onInit();
         dataStreamer.onInit();
@@ -31,20 +42,22 @@ public class Backtester {
         while (dataStreamer.hasNext() || eventConsumer.hasEvents()) {
 
             // Process all events in queue first..
-            while(eventConsumer.hasEvents()){
+            while (eventConsumer.hasEvents()) {
                 Event ev = eventConsumer.getEvent();
-                engine.processEvent(ev);
+                eventEmitter.emit(ev);
             }
 
             // If DataStreamer still has data, Load it..
-            if(dataStreamer.hasNext()){
+            if (dataStreamer.hasNext()) {
                 TickData data = dataStreamer.readData();
                 eventPublisher.publishEvent(new TickEvent(data));
             }
         }
 
-//        eventEmitter.unregister(TickEvent.class, tickListener);
+        eventEmitter.unregister(TickEvent.class, tickListener);
         dataStreamer.onComplete();
         bt.onComplete();
+
+        this.reset();
     }
 }

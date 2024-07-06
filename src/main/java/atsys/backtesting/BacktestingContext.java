84395ffaf284abece097edb6a90c5a9999dc9456
Dhruv Verma <dhruvv90@gmail.com>
2024-07-07 03:01:42 +0530

    Move EventsRepository

--- src/main/java/atsys/backtesting/BacktestingContext.java
+++ src/main/java/atsys/backtesting/BacktestingContext.java
@@ -2,7 +2,12 @@ package atsys.backtesting;
 
 
 import atsys.backtesting.engine.EventPublisher;
+import atsys.backtesting.engine.EventsRepository;
 import atsys.backtesting.engine.events.Event;
+import atsys.backtesting.engine.events.SignalEvent;
+import atsys.backtesting.engine.events.TickEvent;
+import atsys.backtesting.engine.listeners.SignalEventListener;
+import atsys.backtesting.engine.listeners.TickEventListener;
 import atsys.backtesting.model.Backtest;
 import lombok.Getter;
 
@@ -13,12 +18,19 @@ public class BacktestingContext {
 
     private final EventPublisher eventPublisher;
 
-    public BacktestingContext(Backtest backtest, EventPublisher eventPublisher){
+    public BacktestingContext(Backtest backtest, EventPublisher eventPublisher, EventsRepository eventsRepository){
         this.backtest = backtest;
         this.eventPublisher = eventPublisher;
 
         this.backtest.getStrategy().onInit(this);
         this.backtest.getPortfolioManager().onInit(this);
+
+
+        // Register Strategy as TickEventListener
+        eventsRepository.register(TickEvent.class, new TickEventListener(backtest.getStrategy()));
+
+        // Register PortfolioManager
+        eventsRepository.register(SignalEvent.class, new SignalEventListener(backtest.getPortfolioManager()));
     }
 
 
--- src/main/java/atsys/backtesting/engine/EventDrivenEngine.java
+++ src/main/java/atsys/backtesting/engine/EventDrivenEngine.java
@@ -3,19 +3,9 @@ package atsys.backtesting.engine;
 import atsys.backtesting.BacktestingContext;
 import atsys.backtesting.engine.events.Event;
 import atsys.backtesting.engine.events.KillEvent;
-import atsys.backtesting.engine.events.SignalEvent;
-import atsys.backtesting.engine.events.TickEvent;
-import atsys.backtesting.engine.listeners.EventListener;
 import atsys.backtesting.engine.listeners.KillEventListener;
-import atsys.backtesting.engine.listeners.SignalEventListener;
-import atsys.backtesting.engine.listeners.TickEventListener;
 import atsys.backtesting.model.Backtest;
 import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * A Re-usable Event Driven Engine used by the backtester
@@ -54,53 +44,9 @@ public class EventDrivenEngine {
     }
 
     public void initializeForBacktest(Backtest backtest) {
-        currentContext = new BacktestingContext(backtest, publisher);
-
-        // Register Strategy as TickEventListener
-        eventsRepository.register(TickEvent.class, new TickEventListener(backtest.getStrategy()));
+        currentContext = new BacktestingContext(backtest, publisher, eventsRepository);
 
         // Register KillEvent
         eventsRepository.register(KillEvent.class, new KillEventListener());
-
-        // Register PortfolioManager
-        eventsRepository.register(SignalEvent.class, new SignalEventListener(backtest.getPortfolioManager()));
-    }
-
-
-    private static class EventsRepository {
-        private final Map<Class<? extends Event>, List<EventListener<? extends Event>>> listenersMap;
-
-        EventsRepository() {
-            this.listenersMap = new HashMap<>();
-        }
-
-        public <E extends Event> void register(Class<E> clazz, EventListener<E> listener) {
-            listenersMap.computeIfAbsent(clazz, l -> new ArrayList<>()).add(listener);
-        }
-
-        public <E extends Event> void unregister(Class<E> clazz, EventListener<E> listener) {
-            List<EventListener<? extends Event>> listenerValues = listenersMap.get(clazz);
-            if (listenerValues != null) {
-                listenerValues.remove(listener);
-                if (listenerValues.isEmpty()) {
-                    listenersMap.remove(clazz);
-                }
-            }
-        }
-
-        public void unregisterAll() {
-            listenersMap.clear();
-        }
-
-
-        @SuppressWarnings("unchecked")
-        public <E extends Event> void emit(E event) {
-            List<EventListener<? extends Event>> listeners = listenersMap.get(event.getClass());
-            if (listeners != null) {
-                for (EventListener<? extends Event> listener : listeners) {
-                    ((EventListener<E>) listener).onEvent(event);
-                }
-            }
-        }
     }
 }

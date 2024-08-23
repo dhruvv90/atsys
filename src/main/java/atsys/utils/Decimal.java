705a53677b72b45c7536437be1d5e4fcd04f2e89
Dhruv Verma <dhruvv90@gmail.com>
2024-08-24 01:14:16 +0530

    Add Decimal class for numbers handling

---
+++ src/main/java/atsys/utils/Decimal.java
@@ -0,0 +1,160 @@
+package atsys.utils;
+
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public class Decimal {
+
+    @Getter
+    private final BigDecimal value;
+    private static final int DEFAULT_SCALE = 6;
+    private static final RoundingMode DEFAULT_ROUNDING_MODE = RoundingMode.HALF_EVEN;
+
+    // Constructors
+    private Decimal(BigDecimal value) {
+        this.value = value.setScale(DEFAULT_SCALE, DEFAULT_ROUNDING_MODE);
+    }
+
+    private Decimal(double value) {
+        this(BigDecimal.valueOf(value));
+    }
+
+    private Decimal(String value) {
+        this(new BigDecimal(value));
+    }
+
+    private Decimal(int value) {
+        this(BigDecimal.valueOf(value));
+    }
+
+    // Creating objects through static methods
+    public static Decimal valueOf(String value) {
+        return new Decimal(value);
+    }
+
+    public static Decimal valueOf(int value) {
+        return new Decimal(value);
+    }
+
+    public static Decimal valueOf(double value) {
+        return new Decimal(value);
+    }
+
+    public static Decimal valueOf(BigDecimal value) {
+        return new Decimal(value);
+    }
+
+    // Arithmetic operations
+    public Decimal add(Decimal other) {
+        return Decimal.valueOf(value.add(other.value));
+    }
+
+    public Decimal add(BigDecimal other) {
+        return add(Decimal.valueOf(other));
+    }
+
+    public Decimal add(double other) {
+        return add(Decimal.valueOf(other));
+    }
+
+    public Decimal add(int other) {
+        return add(Decimal.valueOf(other));
+    }
+
+    public Decimal subtract(Decimal other) {
+        return Decimal.valueOf(this.value.subtract(other.value));
+    }
+
+    public Decimal subtract(BigDecimal other) {
+        return subtract(Decimal.valueOf(other));
+    }
+
+    public Decimal subtract(double other) {
+        return subtract(Decimal.valueOf(other));
+    }
+
+    public Decimal subtract(int other) {
+        return subtract(Decimal.valueOf(other));
+    }
+
+    public Decimal multiply(Decimal multiplier) {
+        return Decimal.valueOf(this.value.multiply(multiplier.value));
+    }
+
+    public Decimal multiply(BigDecimal multiplier) {
+        return multiply(Decimal.valueOf(multiplier));
+    }
+
+    public Decimal multiply(double multiplier) {
+        return multiply(Decimal.valueOf(multiplier));
+    }
+
+    public Decimal multiply(int multiplier) {
+        return multiply(Decimal.valueOf(multiplier));
+    }
+
+    public Decimal divide(Decimal divisor) {
+        return Decimal.valueOf(this.value.divide(divisor.value, DEFAULT_SCALE, DEFAULT_ROUNDING_MODE));
+    }
+
+    public Decimal divide(BigDecimal divisor) {
+        return divide(Decimal.valueOf(divisor));
+    }
+
+    public Decimal divide(double divisor) {
+        return divide(Decimal.valueOf(divisor));
+    }
+
+    public Decimal divide(int divisor) {
+        return divide(Decimal.valueOf(divisor));
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    public boolean equals(Integer o) {
+        return equals(Decimal.valueOf(o));
+    }
+
+    public boolean equals(BigDecimal o) {
+        return equals(Decimal.valueOf(o));
+    }
+
+    public boolean equals(Double o) {
+        return equals(Decimal.valueOf(o));
+    }
+
+    public boolean equals(Decimal o) {
+        return Objects.equals(value, o.value);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+
+        if (getClass().equals(o.getClass())) {
+            return equals((Decimal)o);
+        } else if (Integer.class.equals(o.getClass())) {
+            return equals((Integer) o);
+        } else if (Double.class.equals(o.getClass())) {
+            return equals((Double) o);
+        } else if (BigDecimal.class.equals(o.getClass())) {
+            return equals((BigDecimal) o);
+        }
+        return false;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/atsys/backtesting/DecimalTest.java
@@ -0,0 +1,83 @@
+package atsys.backtesting;
+
+import atsys.backtesting.engine.exception.BaseException;
+import atsys.utils.Decimal;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecimalTest {
+
+    @Test
+    void BasicTest() throws BaseException {
+        BigDecimal one = BigDecimal.valueOf(1.0);
+        BigDecimal two = BigDecimal.valueOf(1);
+        BigDecimal three = BigDecimal.valueOf(1.000000);
+        BigDecimal four = BigDecimal.valueOf(1.0000001);
+        BigDecimal five = BigDecimal.valueOf(1.000001);
+
+
+        assertNotEquals(one, two);
+
+        Decimal decimal1 = Decimal.valueOf(one);
+        Decimal decimal2 = Decimal.valueOf(two);
+        Decimal decimal3 = Decimal.valueOf(three);
+        Decimal decimal4 = Decimal.valueOf(four);
+        Decimal decimal5 = Decimal.valueOf(five);
+
+        assertEquals(decimal1, decimal2);
+        assertEquals(decimal2, decimal3);
+        assertEquals(decimal3, decimal4);
+        assertNotEquals(decimal4, decimal5);
+    }
+
+    @Test
+    void TypesTest() {
+        BigDecimal one = BigDecimal.valueOf(2);
+        double two = 2.00;
+        double three = 2.000;
+        BigDecimal four = BigDecimal.valueOf(2.0000000000001);
+
+        assertEquals(Decimal.valueOf(one), Decimal.valueOf(two));
+        assertEquals(Decimal.valueOf(two), Decimal.valueOf(three));
+        assertEquals(Decimal.valueOf(three), Decimal.valueOf(four));
+
+        assertEquals(Decimal.valueOf(2.220), Decimal.valueOf(2.2200));
+        assertNotEquals(Decimal.valueOf(2.220), Decimal.valueOf(2.2201));
+        assertEquals(Decimal.valueOf(2.000000), Decimal.valueOf(2.0000001));
+
+        assertEquals(Decimal.valueOf("1.0"), Decimal.valueOf(1.0));
+        assertEquals(Decimal.valueOf(1), Decimal.valueOf(1.000));
+    }
+
+    @Test
+    void arithmeticTest() {
+        assertEquals(Decimal.valueOf(2.00).add(1), Decimal.valueOf(3));
+        assertEquals(Decimal.valueOf(2.00).add(1.0000001), Decimal.valueOf(3));
+        assertNotEquals(Decimal.valueOf(2.00).add(1.000001), Decimal.valueOf(3));
+
+        assertEquals(Decimal.valueOf(2.01).multiply(2), Decimal.valueOf(4.02));
+        assertEquals(Decimal.valueOf(2.0000000000001).multiply(2), Decimal.valueOf(4.00));
+
+        int test = 1;
+        assertTrue(Decimal.valueOf(1.00).equals(test));
+
+        assertTrue(Decimal.valueOf(1.1).add(2.2).equals(3.3000000001));
+    }
+
+    @Test
+    void equalityTest(){
+        assertTrue(Decimal.valueOf(2).equals(2.000));
+        assertTrue(Decimal.valueOf(2).equals(BigDecimal.valueOf(2.00000)));
+        assertTrue(Decimal.valueOf(2.00000).equals(2));
+    }
+
+    @Test
+    void multiplyTest(){
+        assertTrue(Decimal.valueOf(2).multiply(2.1).equals(4.200));
+        assertTrue(Decimal.valueOf(10).divide(3).equals(3.333333));
+        assertTrue(Decimal.valueOf(100).divide(30).equals(Decimal.valueOf(1).divide(0.3)));
+    }
+}
